3848737ec45c35b82865e104820878403d9cd3b4
Paul Ralyea <047par@gmail.com>
2020-03-12 18:11:34 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -15,3 +15,47 @@ def directors_totals(nds)
   result = {}
   nil
 end
+
+def directors_totals(nds)
+   result = {}
+   director_index = 0
+  while director_index < nds.size do
+    director = nds[director_index]
+    result[director[:name]] = gross_for_director(director)
+    director_index += 1
+  end
+   result
+  end
+   
+   def gross_for_director(d)
+   total = 0
+   index = 0
+  while index < d[:movies].length do
+     total += d[:movies][index][:worldwide_gross]
+     index += 1
+   end
+
+   total
+ end
+ 
+ def list_of_directors(source)
+   director_index = 0
+   director_list = Array.new
+
+   while director_index < source.size do
+     director_list.push(source[director_index][:name])
+     director_index += 1
+   end
+   director_list
+ end
+ 
+ def total_gross(source)
+   total_gross_of_earnings = 0
+   director_index = 0
+
+   while director_index < source.size do
+     total_gross_of_earnings += gross_for_director( source[director_index] )
+     director_index += 1
+   end
+ total_gross_of_earnings
+ end
